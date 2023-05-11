c5a9fc3cdabea2ba17b4384c081ead131061a038
Denys Denysenko <ddenisenko1989@gmail.com>
2023-05-11 13:28:08 +0300

    Updates

--- _app/immutable/chunks/stores.102ad031.js
+++
@@ -1,1 +0,0 @@
-import"./index.00e67d7c.js";import{s as e}from"./singletons.c0e638e8.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
