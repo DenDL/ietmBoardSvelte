70da9594ee89c6cdc65b68b70424cd95176e0c01
denis <ddenisenko1989@gmail.com>
2023-05-11 12:44:34 +0300

    itemBoardSvelte

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -9,11 +9,11 @@
 			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
 		</svg>
 		<ul class="flex justify-center">
-			<li aria-current={$page.url.pathname === base + '/' ? 'page' : undefined}>
-				<a href="{base}/">Gallery Grid</a>
+			<li aria-current={$page.url.pathname === base ? 'page' : undefined}>
+				<a href="{base}">Gallery Grid</a>
 			</li>
 			<li aria-current={$page.url.pathname === base + '/bigImg' ? 'page' : undefined}>
-				<a href="{base}/bigImg">Big image</a>
+				<a href="{base + '/bigImg'}">Big image</a>
 			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
